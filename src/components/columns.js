2a7b08e523df02397e8b3ddd5ccb22ffbfe9cf9c
meha-jain <41738856+meha-jain@users.noreply.github.com>
2021-10-19 01:10:05 +0530

    Add files via upload

---
+++ src/components/columns.js
@@ -0,0 +1,8 @@
+export const cols =  [
+   
+        { Header: "First Name",label:"fullName", key: "fullName", accessor:'fullName' },
+        {  Header:'Address', label: "address", key: "address", accessor:'address' },
+        {  Header:'Phone Number', label: "phoneNumber", key: "phoneNumber", accessor:'phoneNumber' },
+        {  Header:'E mail', label: "email", key: "email", accessor:'email' }
+        
+      ];
